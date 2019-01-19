bc78d2cf89c41729e36dec30c3ee8c448aba0f70
Robert Worman <tr3ppy@gmail.com>
2019-01-19 12:12:55 +1000

    Skeleton modal bindings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,20 +11,26 @@
       <div class="row center-xs">
         <div class="col-xs-3">
           <!-- create new button -->
-          <div class="icon-button">
+          <div class="icon-button" @click="showCreateModal = true">
             <i class="icon-button__icon mdi mdi-file-tree"></i>
             <div class="icon-button__caption">Create new tree.</div>
           </div>
         </div>
         <div class="col-xs-3">
           <!-- open existing button -->
-          <div class="icon-button">
+          <div class="icon-button" @click="showOpenModal = true">
             <i class="icon-button__icon mdi mdi-book-open-variant"></i>
             <div class="icon-button__caption">Open a previously saved tree.</div>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- create new tree modal -->
+    <div class="modal" v-if="showCreateModal">Enter the name for your family tree: [ ]</div>
+
+    <!-- open existing modal -->
+    <div class="modal" v-if="showOpenModal">Select from the following saved trees: [ ]</div>
   </div>
 </template>
 
@@ -32,5 +38,8 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  showCreateModal: boolean = false
+  showOpenModal: boolean = false
+}
 </script>
