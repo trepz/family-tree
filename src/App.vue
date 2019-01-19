71d5d41da2907d63dfa725728186069e7913ffd1
Robert Worman <tr3ppy@gmail.com>
2019-01-19 10:48:55 +1000

    Scrap sidebar concept

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-    <Sidebar/>
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import Sidebar from '@/components/Sidebar.vue'
 
-@Component({
-  components: { Sidebar },
-})
+@Component
 export default class App extends Vue {}
 </script>
 
--- tests/unit/sidebar.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import Sidebar from '@/components/Sidebar.vue'
-
-describe('Sidebar.vue', () => {
-  it('is vue', () => {
-    const wrapper = shallowMount(Sidebar)
-    expect(wrapper.isVueInstance()).toBe(true)
-  })
-})
