8644c7e38e9f7ad6e726505a47e81206a4789a54
Robert Worman <tr3ppy@gmail.com>
2019-01-19 12:08:24 +1000

    Remove dud import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,7 +30,6 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component
 export default class Home extends Vue {}
