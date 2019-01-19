8524e6f9669c8480c381e0d6fc8d0a9fda7d991f
Robert Worman <tr3ppy@gmail.com>
2019-01-19 11:18:35 +1000

    Basic home structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="page">
+    <div class="page__content page__content--centered">
+      <!-- main title -->
+      <div class="title title--big">Family Tree.</div>
+
+      <!-- init buttons -->
+      <div class="row center-xs">
+        <div class="col-xs-3">New</div>
+        <div class="col-xs-3">Open</div>
+      </div>
+    </div>
   </div>
 </template>
 
