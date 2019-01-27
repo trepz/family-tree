4706b09a455aefc5d4427aa510a03623657cdfd2
Robert Worman <tr3ppy@gmail.com>
2019-01-28 09:58:16 +1000

    Use literal tag in app component

--- src/App.lit.ts
+++ src/App.lit.ts
@@ -1,11 +1,12 @@
 import { Vue, Component } from 'vue-property-decorator'
+import { html } from '@/utils'
 
 /** @VueLiteralCompiler Template */
-const template = `<template>
-  <div id="app">
-    <router-view/>
-  </div>
-</template>`
+const template = html`
+  <template>
+    <div id="app"><router-view /></div>
+  </template>
+`
 
 @Component
 export default class App extends Vue {}
