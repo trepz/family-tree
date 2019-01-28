b7bc950d53afc9062540b3b603940cd0bf346461
Robert Worman <tr3ppy@gmail.com>
2019-01-28 11:46:48 +1000

    Add close event emitter to modal

--- src/components/Modal.lit.ts
+++ src/components/Modal.lit.ts
@@ -1,14 +1,14 @@
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Emit } from 'vue-property-decorator'
 import { html } from '@/utils'
 
 /** @VueLiteralCompiler Template */
-const template = html`
+const template = (modal: Modal) => html`
   <div class="modal is-active">
-    <div class="modal-background"></div>
+    <div class="modal-background" @click="${modal.close()}"></div>
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">Modal title</p>
-        <button class="delete" aria-label="close"></button>
+        <button class="delete" aria-label="close" @click="${modal.close()}"></button>
       </header>
       <section class="modal-card-body"><slot></slot></section>
       <footer class="modal-card-foot">
@@ -19,4 +19,9 @@ const template = html`
 `
 
 @Component
-export default class Modal extends Vue {}
+export default class Modal extends Vue {
+  @Emit()
+  close() {
+    return
+  }
+}
