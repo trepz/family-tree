4318af4a8119031967e11d1596b889b6ff697a0a
Robert Worman <tr3ppy@gmail.com>
2019-01-28 12:01:39 +1000

    Modal receive array of buttons as props

--- src/components/Modal.lit.ts
+++ src/components/Modal.lit.ts
@@ -12,16 +12,33 @@ const template = (modal: Modal) => html`
       </header>
       <section class="modal-card-body"><slot></slot></section>
       <footer class="modal-card-foot">
-        <button class="button is-success">Save changes</button> <button class="button">Cancel</button>
+        <button v-for="b in buttons" class="button" :class="b.color" @click="$emit(b.event)">{{ b.label }}</button>
       </footer>
     </div>
   </div>
 `
+type ModalButton = {
+  label: string
+  event: string
+  color?: string
+}
 
 @Component
 export default class Modal extends Vue {
   @Prop({ type: String, default: '' })
   title!: string
+
+  @Prop({
+    type: Array,
+    default: () => [
+      {
+        label: 'Ok',
+        event: 'close',
+        color: 'is-success',
+      },
+    ],
+  })
+  buttons!: ModalButton[]
 
   @Emit()
   close() {
